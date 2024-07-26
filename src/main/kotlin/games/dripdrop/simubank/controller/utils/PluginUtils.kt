097fd1855e3715bdae94cf90d44133b1c4de4753
mac <DripDropMC@outlook.com>
2024-07-26 23:27:38 +0800

    feat(all): add Bukkit plugin config

--- src/main/kotlin/games/dripdrop/simubank/controller/utils/PluginUtils.kt
+++ src/main/kotlin/games/dripdrop/simubank/controller/utils/PluginUtils.kt
@@ -1,5 +1,73 @@
 package games.dripdrop.simubank.controller.utils
 
-object PluginUtils {
-    const val COMMAND = "ddbank"
+
+import games.dripdrop.simubank.DripDropBank
+import games.dripdrop.simubank.model.constant.FileEnums
+import org.bukkit.Bukkit
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.nio.file.Files
+
+const val COMMAND = "ddbank"
+private var mConfiguration: YamlConfiguration? = null
+private val mLogger = LoggerFactory.getLogger(DripDropBank::class.java.simpleName)
+
+fun runSyncTask(plugin: JavaPlugin, action: () -> Unit) {
+    Bukkit.getScheduler().runTask(plugin, action)
+}
+
+fun runAsyncTask(plugin: JavaPlugin, action: () -> Unit) {
+    Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable { action() })
+}
+
+fun d(msg: String) = mLogger.debug(msg)
+
+fun i(msg: String) = mLogger.info(msg)
+
+fun w(msg: String) = mLogger.warn(msg)
+
+fun e(msg: String) = mLogger.error(msg)
+
+fun getPluginConfig(): YamlConfiguration? {
+    i("is configuration null: ${mConfiguration == null}")
+    return mConfiguration
+}
+
+fun getConfigYaml(plugin: JavaPlugin) {
+    if (!checkFileOrDirectoryAvailable(plugin.dataFolder, FileEnums.CONFIG)) {
+        return
+    }
+    try {
+        val file = File("${plugin.dataFolder}${File.separator}${FileEnums.CONFIG.file}")
+        i("config file path is [${file.path}]")
+        mConfiguration = YamlConfiguration.loadConfiguration(file)
+    } catch (e: Exception) {
+        e("failed to get config yaml file: ${e.localizedMessage}")
+        e.printStackTrace()
+    }
+}
+
+fun checkFileOrDirectoryAvailable(directory: File, file: FileEnums): Boolean {
+    i("checkFileOrDirectoryAvailable called: ${directory.path}")
+    return try {
+        if (!directory.exists()) {
+            i("try to make directory [$directory]")
+            directory.mkdir().apply { i("result = $this") }
+        }
+        i("directory [$directory] exists: ${directory.exists()}")
+        val f = File(directory, file.file)
+        i("file [$f] exists: ${f.exists()}")
+        if (directory.exists() && !f.exists()) {
+            DripDropBank::class.java.getResourceAsStream("/${file.file}")?.let {
+                Files.copy(it, f.toPath()).apply { i("file copy result: $this") }
+            }
+        }
+        f.exists()
+    } catch (e: Exception) {
+        e("failed to check or copy file: ${e.localizedMessage}")
+        e.printStackTrace()
+        false
+    }
 }
---
+++ src/main/kotlin/games/dripdrop/simubank/model/constant/FileEnums.kt
@@ -0,0 +1,7 @@
+package games.dripdrop.simubank.model.constant
+
+enum class FileEnums(val file: String) {
+    CONFIG("config.yml"),
+    LANG("lang.yml"),
+    PRODUCTS("products.yml")
+}
